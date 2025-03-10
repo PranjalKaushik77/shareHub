6c84143596a4f38ef2489941030f4d254b3f1aa2
PranjalKaushik7 <pranjalkaushik354@gmail.com>
2025-03-10 12:01:01 +0530

    initial commit, javascript code

---
+++ public/config.js
@@ -0,0 +1,10 @@
+// Cloudinary configuration
+const cloudinaryConfig = {
+  cloudName: "dq7id80yr", // Replace with your Cloudinary cloud name
+  apiKey: "148382145222729", // Replace with your Cloudinary API key
+  apiSecret: "ROaKrE0-qgZtg5E0jZPICBWlPzY", // Replace with your Cloudinary API secret
+  uploadPreset: "shareHub", // Replace with your upload preset (optional)
+}
+
+// Export the configuration
+export default cloudinaryConfig
